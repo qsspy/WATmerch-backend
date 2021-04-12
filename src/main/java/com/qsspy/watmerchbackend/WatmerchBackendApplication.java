361af4870ab4bd3aa68c4ff3c0597a99d2df237d
Daniel <danel.filipek@gmail.com>
2021-04-12 15:39:21 +0200

    Initial commit

---
+++ src/main/java/com/qsspy/watmerchbackend/WatmerchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.qsspy.watmerchbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatmerchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatmerchBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qsspy/watmerchbackend/WatmerchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qsspy.watmerchbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatmerchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
